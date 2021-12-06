6f1cea84de192266b8e4d342ea6cc473e0a2874e
warddam001 <warddam001@warren.k12.in.us>
2021-12-06 13:21:58 -0500

    U3 project

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,6 +1,22 @@
+import javax.swing.*;
+
 public class EscapeRoom {
 
+    JPanel panel;
+
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Escape");
+        frame.setSize(400, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        panel = new EmojiPanel();
+        JButton smile = new JButton("Click to Smile");
+        JButton frown = new JButton("Click to Frown");
+
+        panel.add(smile);
+        panel.add(frown);
+
 
     }
 
