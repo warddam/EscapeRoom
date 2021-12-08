db18fa1f9506c08155ea5df7af50a3431f1dc1bf
warddam001 <warddam001@warren.k12.in.us>
2021-12-08 13:18:32 -0500

    U3 project

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,21 +1,42 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 
 public class EscapeRoom {
 
-    JPanel panel;
+
+    private static JPanel panel;
+    JButton createProblem, checkAnswer;
+    JLabel showProblem;
+    JTextField answerInput;
+    JRadioButton addition, subtraction, multiplication, division;
+    ButtonGroup radios = new ButtonGroup();
 
     public static void main(String[] args) {
+
         JFrame frame = new JFrame("Escape");
         frame.setSize(400, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
-        panel = new EmojiPanel();
-        JButton smile = new JButton("Click to Smile");
-        JButton frown = new JButton("Click to Frown");
+        addition = new  JRadioButton("Additon");
+        subtraction = new JRadioButton("Subtraction");
+        multiplication = new JRadioButton("multiplication");
+        division = new JRadioButton("division");
 
-        panel.add(smile);
-        panel.add(frown);
+
+        panel = new JPanel();
+        JButton Escape = new JButton("Click to Smile");
+        JButton calculator = new JButton("Click to Frown");
+
+        panel.add(Escape);
+        panel.add(calculator);
+
+    frame.add(panel);
+    frame.setVisible(true);
 
 
     }
