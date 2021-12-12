e956ee0b8c680ed9f971bdb94d1b7bdb35e68feb
DJ Ward <warddam001@warren.k12.in.us>
2021-12-12 17:43:18 -0500

    u3 projects

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,23 +1,93 @@
-import javax.swing.*;
+import javafx.scene.shape.Shape3D;
+import org.w3c.dom.css.RGBColor;
 
-public class EscapeRoom {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
-    JPanel panel;
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Escape");
-        frame.setSize(400, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
 
-        panel = new EmojiPanel();
-        JButton smile = new JButton("Click to Smile");
-        JButton frown = new JButton("Click to Frown");
 
-        panel.add(smile);
-        panel.add(frown);
+public class EscapeRoom extends Frame implements ActionListener {
+    Button d1, d2, d3, d4;
+
+
+    EscapeRoom() {
+        super("Escape Room");
+        d1 = new Button("Wacky Door");
+        d1.setBounds(50, 100, 80, 80);
+        d1.setBackground(new java.awt.Color(218,165,32));
+        d2 = new Button("Lazy Door");
+        d2.setBounds(200, 100, 80, 80);
+        d2.setBackground(new java.awt.Color(218,165,32));
+        d3 = new Button("Lucky Door");
+        d3.setBounds(50, 200, 80, 80);
+        d3.setBackground(new java.awt.Color(218,165,32));
+        d4 = new Button("Mystery Door");
+        d4.setBounds(200, 200, 80, 80);
+        d4.setBackground(new java.awt.Color(218,165,32));
+
+
+        d1.addActionListener(this);
+        d2.addActionListener(this);
+        d3.addActionListener(this);
+        d4.addActionListener(this);
+
+
+        add(d1);
+        add(d2);
+        add(d3);
+        add(d4);
+
+        setSize(400, 400);
+        setLayout(null);
+        setVisible(true);
+        setBackground(Color.black);
+
 
 
     }
+
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == d2) {
+            JOptionPane.showMessageDialog(null, "The key does not fit." + "\n" + "You have failed miserably!!!");
+        }
+        if (e.getSource() == d1) {
+            JOptionPane.showMessageDialog(null, "The key does not fit." + "\n" + "You seriously need to concentrate!!!");
+        }
+        if (e.getSource() == d4) {
+            JOptionPane.showMessageDialog(null, "The key does not fit." + "\n" + "To the dungeon you go!!!");
+        }
+        else if (e.getSource() == d3) {
+            JOptionPane.showMessageDialog(null, "The key fits" + "\n" + "You stuck with you gut and chose luck!!!" +"\n"+"Great Job!");
+        }
+    }
+        public static void main (String[]args){
+
+
+            new EscapeRoom();
+            escapeRoomRules();
+
+
+
+        }
+
+        public static void escapeRoomRules() {
+
+            System.out.println("You have entered the escape room." + "\n" + "Use the key to open the correct door." + "\n" + "If you choose the wrong door you will end up in turmoil.");
+        }
+        public static void correctDoor() {
+
+        boolean d3 = true;
+
+
+
+
+        }
+
 
 }
